56667d782445b74bf5f60456b4cb2a8c887951cb
Bjarne <Bjarne@PC-BJARNE>
2024-07-31 19:29:12 +0200

    EingabeArticle: TextBoxes und Buttons

--- Informatik3/EingabeArticle.h
+++ Informatik3/EingabeArticle.h
@@ -35,6 +35,31 @@ namespace Informatik3 {
 			}
 		}
 
+	private: System::Windows::Forms::TextBox^  textBox1;
+	protected:
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::TextBox^  textBox5;
+	private: System::Windows::Forms::TextBox^  textBox6;
+	private: System::Windows::Forms::TextBox^  textBox7;
+	private: System::Windows::Forms::TextBox^  textBox8;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
+	private: System::Windows::Forms::Label^  label7;
+	private: System::Windows::Forms::Label^  label8;
+	private: System::Windows::Forms::TextBox^  textBox9;
+	private: System::Windows::Forms::TextBox^  textBox10;
+	private: System::Windows::Forms::Label^  label9;
+	private: System::Windows::Forms::Label^  label10;
+
 	private:
 		/// <summary>
 		/// Required designer variable.
@@ -48,12 +73,278 @@ namespace Informatik3 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"EingabeArticle";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->textBox9 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox10 = (gcnew System::Windows::Forms::TextBox());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label10 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(159, 75);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(147, 20);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &EingabeArticle::textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(50, 81);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(47, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"keyword";
+			this->label1->Click += gcnew System::EventHandler(this, &EingabeArticle::label1_Click);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(435, 698);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Abbrechen";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(450, 75);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"Speichern";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &EingabeArticle::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(528, 698);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 4;
+			this->button3->Text = L"Beenden";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(159, 112);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(147, 20);
+			this->textBox2->TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(159, 149);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(147, 20);
+			this->textBox3->TabIndex = 6;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(159, 189);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(147, 20);
+			this->textBox4->TabIndex = 7;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(159, 227);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(147, 20);
+			this->textBox5->TabIndex = 8;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(159, 271);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(147, 20);
+			this->textBox6->TabIndex = 9;
+			// 
+			// textBox7
+			// 
+			this->textBox7->Location = System::Drawing::Point(159, 315);
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(147, 20);
+			this->textBox7->TabIndex = 10;
+			// 
+			// textBox8
+			// 
+			this->textBox8->Location = System::Drawing::Point(159, 361);
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(147, 20);
+			this->textBox8->TabIndex = 11;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(50, 115);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(27, 13);
+			this->label2->TabIndex = 12;
+			this->label2->Text = L"Titel";
+			this->label2->Click += gcnew System::EventHandler(this, &EingabeArticle::label2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(50, 152);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(38, 13);
+			this->label3->TabIndex = 13;
+			this->label3->Text = L"Author";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(50, 192);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(41, 13);
+			this->label4->TabIndex = 14;
+			this->label4->Text = L"Journal";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(50, 230);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(27, 13);
+			this->label5->TabIndex = 15;
+			this->label5->Text = L"Jahr";
+			this->label5->Click += gcnew System::EventHandler(this, &EingabeArticle::label5_Click);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(50, 278);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(42, 13);
+			this->label6->TabIndex = 16;
+			this->label6->Text = L"Volume";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(50, 318);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(46, 13);
+			this->label7->TabIndex = 17;
+			this->label7->Text = L"Nummer";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(50, 364);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(37, 13);
+			this->label8->TabIndex = 18;
+			this->label8->Text = L"Seiten";
+			this->label8->Click += gcnew System::EventHandler(this, &EingabeArticle::label8_Click);
+			// 
+			// textBox9
+			// 
+			this->textBox9->Location = System::Drawing::Point(159, 408);
+			this->textBox9->Name = L"textBox9";
+			this->textBox9->Size = System::Drawing::Size(147, 20);
+			this->textBox9->TabIndex = 19;
+			// 
+			// textBox10
+			// 
+			this->textBox10->Location = System::Drawing::Point(159, 449);
+			this->textBox10->Name = L"textBox10";
+			this->textBox10->Size = System::Drawing::Size(147, 20);
+			this->textBox10->TabIndex = 20;
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(50, 411);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(37, 13);
+			this->label9->TabIndex = 21;
+			this->label9->Text = L"Monat";
+			this->label9->Click += gcnew System::EventHandler(this, &EingabeArticle::label9_Click);
+			// 
+			// label10
+			// 
+			this->label10->AutoSize = true;
+			this->label10->Location = System::Drawing::Point(50, 452);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(61, 13);
+			this->label10->TabIndex = 22;
+			this->label10->Text = L"Anmerkung";
+
+			// 
+			// EingabeArticle
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(631, 733);
+			this->Controls->Add(this->label10);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->textBox10);
+			this->Controls->Add(this->textBox9);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox8);
+			this->Controls->Add(this->textBox7);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"EingabeArticle";
+			this->Text = L"EingabeArticle";
+			this->Load += gcnew System::EventHandler(this, &EingabeArticle::EingabeArticle_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void EingabeArticle_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void label5_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void label8_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void label9_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
 	};
 }
